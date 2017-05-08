f6ddc9e6e18f0648d23be5ea8cf797b640850a92
dav2113534 <dpalacios03081991@gmail.com>
2017-05-08 09:17:15 -0700

    added tests, start + rec functions

--- main.js
+++ main.js
@@ -1,9 +1,68 @@
-var player1 = 0; 
-var player2 = 2;
+/* 
+this will keep each player's score
+*/
+var player1 = 0;
+var player2 = 0;
 
-function player(){
-    board[+tile] = 'X'; 
+
+function start(state) {
+    state.currentPlayer = 'X';
+    state.grid = [
+        [-1, -1, -1],
+        [-1, -1, -1],
+        [-1, -1, -1]
+    ];
+    return state;
+
 }
-function secondPlayer(){
-    board[+title] = 'O'; 
+
+function recMove(state) {
+    if (state.currentPlayer === "O") {
+        state.currentPlayer = "X";
+    } else {
+        state.currentPlayer = "O";
+    }
+    return state;
 }
+
+
+/* TESTS */
+/*** Test Helpers ***/
+function testRecPlayerSwitch() {
+    var testState = start({})
+    testState = recMove(testState);
+    assertEqual(testState.currentPlayer, "O");
+
+    testState = recMove(testState);
+    assertEqual(testState.currentPlayer, "X");
+}
+
+function testRecMoveSaving() {
+    var testState = start({})
+}
+
+function testStart() {
+    var testState = {};
+    testState = start(testState);
+
+    assertEqual(testState.currentPlayer, 'X');
+    assertEqual(testState.grid.length, 3, "grid should be initialised")
+}
+
+function test() {
+    testStart();
+    testRecPlayerSwitch();
+}
+
+function assertEqual(actual, expected, message) {
+    if (actual === expected) {
+        console.log("PASS");
+    } else {
+        if (message === undefined) {
+            message = "";
+        }
+        console.error("FAIL:" + message, arguments.callee.caller.name, actual, expected);
+    }
+}
+
+test();
